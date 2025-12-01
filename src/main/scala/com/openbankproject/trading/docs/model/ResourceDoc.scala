80155d3c0633807752f33c0816b21ae744cd9570
Hongwei <hongwei@tesobe.com>
2025-12-01 15:30:16 +0100

    feature/Add ImplementedByJson and ResourceDocJson case classes to enhance API documentation structure and serialization support

--- src/main/scala/com/openbankproject/trading/docs/model/ResourceDoc.scala
+++ src/main/scala/com/openbankproject/trading/docs/model/ResourceDoc.scala
@@ -3,6 +3,7 @@ package com.openbankproject.trading.docs.model
 import org.http4s.{Request, Response}
 import cats.effect.IO
 import com.openbankproject.trading.http.OBPEndpoint
+import io.circe.Json
 
 /**
   * ResourceDoc aligned with OBP-API structure.
@@ -42,4 +43,51 @@ final case class ResourceDoc(
   require(partialFunctionName.trim.nonEmpty, "partialFunctionName must be non-empty")
   require(requestUrl.trim.nonEmpty, "requestUrl must be non-empty")
   require(requestVerb.trim.nonEmpty, "requestVerb must be non-empty")
+}
+
+/**
+  * Used to describe where an API call is implemented, similar to OBP's ImplementedByJson.
+  */
+final case class ImplementedByJson(
+  version: String,  // Short hand for version, e.g. "v7_0_0"
+  function: String  // The partial function name, e.g. "obpGetOfferPF"
+)
+
+/**
+  * Export-friendly ResourceDocJson, mirroring OBP-API structure for docs/export.
+  */
+final case class ResourceDocJson(
+  operation_id: String,
+  implemented_by: ImplementedByJson,
+  request_url: String,
+  summary: String,
+  description: String,
+  description_markdown: String,
+  example_request_body: Json,
+  success_response_body: Json,
+  error_response_bodies: List[String],
+  tags: List[String],
+  typed_request_body: Json,
+  typed_success_response_body: Json,
+  roles: Option[List[String]] = None,
+  is_featured: Boolean = false,
+  special_instructions: Option[String] = None,
+  specified_url: Option[String] = None,
+  connector_methods: List[String] = Nil,
+  created_by_bank_id: Option[String] = None
+)
+
+object ResourceDocJson {
+  import io.circe.generic.semiauto._
+  import io.circe.{Encoder, Decoder}
+
+  implicit val implementedByJsonEncoder: Encoder[ImplementedByJson] =
+    deriveEncoder[ImplementedByJson]
+  implicit val implementedByJsonDecoder: Decoder[ImplementedByJson] =
+    deriveDecoder[ImplementedByJson]
+
+  implicit val resourceDocJsonEncoder: Encoder[ResourceDocJson] =
+    deriveEncoder[ResourceDocJson]
+  implicit val resourceDocJsonDecoder: Decoder[ResourceDocJson] =
+    deriveDecoder[ResourceDocJson]
 }
